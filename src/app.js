5595d488129060e88027890366211cc9f93f0ae5
Jhonttt <juanatahonadev@gmail.com>
2026-01-16 03:25:27 +0100

    fix(cors): habilitar CORS con credenciales para la interfaz de React

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors'); // <-- AÑADIR ESTO
 const authRoutes = require('./routes/authRoutes');
-const authenticate = require('./middleware/authMiddleware'); // <-- IMPORTANTE
+const authenticate = require('./middleware/authMiddleware');
 
 const app = express();
+
+// CORS CONFIG (OBLIGATORIO PARA LOGIN DESDE REACT)
+app.use(cors({
+  origin: 'http://localhost:5173', // frontend
+  credentials: true,               // permitir cookies
+}));
 
 app.use(express.json());
 app.use(cookieParser());
