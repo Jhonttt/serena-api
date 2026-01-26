9c80bf71828a3277db949f7a8535ad21eb76028d
Jhonttt <juanatahonadev@gmail.com>
2026-01-26 01:06:34 +0100

    feat: Añadir validaciones, una nueva ruta para que la reciba el frontend, nueva función en el controlador llamada verifiToken y cambiar ruta raiz a /api

--- src/middleware/validateToken.js
+++ src/middleware/validateToken.js
@@ -4,10 +4,10 @@ import { TOKEN_SECRET } from "../config/config.js";
 export const authRequired = (req, res, next) => {
   const { token } = req.cookies;
 
-  if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+  if (!token) return res.status(401).json(["No token, authorization denied"]);
 
   jwt.verify(token, TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(401).json({ message: "Invalid token" });
+    if (err) return res.status(401).json(["Invalid token"]);
 
     req.user = user;
 
